dcf730a2c051ad731a6fc0f607e71743cf5b3d45
Max Goodman <maximgoodman@gmail.com>
2020-01-13 06:41:56 -0600

    inisitla repo set up

---
+++ src/main/java/com/maximgoodman/pupapp/PupAppApplication.java
@@ -0,0 +1,13 @@
+package com.maximgoodman.pupapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PupAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PupAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maximgoodman/pupapp/PupAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maximgoodman.pupapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PupAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
